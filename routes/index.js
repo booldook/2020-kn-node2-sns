aa1db401ac53e45d3b5a6b95ffbece3da91e3cd6
booldook <booldook@gmail.com>
2020-03-15 16:22:23 +0900

    index router

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var { connect } = require('../modules/mysql');
+var { isLogin, isLogout } = require("../modules/auth-chk");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async (req, res, next) => {
   // req.app.locals.user = req.user;
-  
-  res.render('index', { user: req.user });
+  let sql, result;
+  sql = "SELECT * FROM post ORDER BY id DESC";
+  result = await connect.execute(sql);
+  res.render('index', { user: req.user, lists: result[0] });
 });
 
 module.exports = router;
