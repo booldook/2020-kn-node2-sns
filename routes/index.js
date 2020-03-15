ee88f4a87fadf2d1ce8fc540cdd3680923aad0d1
booldook <booldook@gmail.com>
2020-03-15 16:45:43 +0900

    완성

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var { isLogin, isLogout } = require("../modules/auth-chk");
 router.get('/', async (req, res, next) => {
   // req.app.locals.user = req.user;
   let sql, result;
-  sql = "SELECT * FROM post ORDER BY id DESC";
+  sql = "SELECT * FROM post LEFT JOIN user ON post.user_id = user.id ORDER BY post.id DESC";
   result = await connect.execute(sql);
   res.render('index', { user: req.user, lists: result[0] });
 });
